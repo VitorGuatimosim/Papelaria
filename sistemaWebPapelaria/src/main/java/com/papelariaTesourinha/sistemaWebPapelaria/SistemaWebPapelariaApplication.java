d64e573c494ba75ae06592958883bc8bfc139c40
Vitor Guatimosim <vitor.messagi@gmail.com>
2024-02-22 13:38:51 -0300

    Criação do projeto com back-end

---
+++ sistemaWebPapelaria/src/main/java/com/papelariaTesourinha/sistemaWebPapelaria/SistemaWebPapelariaApplication.java
@@ -0,0 +1,13 @@
+package com.papelariaTesourinha.sistemaWebPapelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaWebPapelariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaWebPapelariaApplication.class, args);
+	}
+
+}
---
+++ sistemaWebPapelaria/src/test/java/com/papelariaTesourinha/sistemaWebPapelaria/SistemaWebPapelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papelariaTesourinha.sistemaWebPapelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaWebPapelariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
