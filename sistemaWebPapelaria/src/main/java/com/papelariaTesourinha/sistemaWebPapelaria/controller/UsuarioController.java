8f79aaa5125becfedfd48c23e5c945060aa7633a
Vitor Guatimosim <vitor.messagi@gmail.com>
2024-02-26 21:37:12 -0300

    Adicionado listagem e pesquisa de usuário
    Sem interação com o usuário, apenas a listagem de dados no banco através do sistema.

--- sistemaWebPapelaria/src/main/java/com/papelariaTesourinha/sistemaWebPapelaria/controller/UsuarioController.java
+++ sistemaWebPapelaria/src/main/java/com/papelariaTesourinha/sistemaWebPapelaria/controller/UsuarioController.java
@@ -2,9 +2,12 @@ package com.papelariaTesourinha.sistemaWebPapelaria.controller;
 
 import com.papelariaTesourinha.sistemaWebPapelaria.data.UsuarioEntity;
 import com.papelariaTesourinha.sistemaWebPapelaria.service.UsuarioService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +28,17 @@ public class UsuarioController {
         var novoUsuario = usuarioService.criarUsuario(user);
         
         return new ResponseEntity<>(novoUsuario, HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/listar")
+    public ResponseEntity<List> getAllUsuarios(){
+        List<UsuarioEntity> usuarios = usuarioService.listarTodosUsuarios();
+        return new ResponseEntity<>(usuarios, HttpStatus.OK);
+    }
+    
+    @GetMapping("/pesquisar/{id}")
+    public  ResponseEntity<UsuarioEntity> getUsuarioById(@PathVariable Integer id){
+        UsuarioEntity usuario = usuarioService.getUsuarioId(id);
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 }
--- sistemaWebPapelaria/src/main/java/com/papelariaTesourinha/sistemaWebPapelaria/service/UsuarioService.java
+++ sistemaWebPapelaria/src/main/java/com/papelariaTesourinha/sistemaWebPapelaria/service/UsuarioService.java
@@ -2,6 +2,7 @@ package com.papelariaTesourinha.sistemaWebPapelaria.service;
 
 import com.papelariaTesourinha.sistemaWebPapelaria.data.UsuarioEntity;
 import com.papelariaTesourinha.sistemaWebPapelaria.data.UsuarioRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,13 @@ public class UsuarioService {
         usuarioRepository.save(user);
         
         return user;
+    }
+    
+     public UsuarioEntity getUsuarioId(Integer userId){
+        return usuarioRepository.findById(userId).orElse(null);
+    }
+    
+    public List<UsuarioEntity> listarTodosUsuarios(){
+        return usuarioRepository.findAll();
     }
 }
